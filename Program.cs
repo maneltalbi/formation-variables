ce580a14885b33938bb057b0d21ab16809659f21
talbi manel <talbimanel20@gmail.com>
2021-11-25 13:26:06 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+            ConsoleWriteLine(y);
+            ConsoleReadLine();
+            */
+            Console.WriteLine("what is your name?");
+            Console.Write("type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("type your last name: ");
+            myLastName = Console.ReadLine();
+            Console.WriteLine("hello, " + myFirstName + "" + myLastName);
+
+        }
+    }
+}
